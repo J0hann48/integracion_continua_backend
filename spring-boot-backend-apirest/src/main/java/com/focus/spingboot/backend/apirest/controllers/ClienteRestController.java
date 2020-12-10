50a8bdd8e981693de147e32bc8114eca60af532f
J0hann48 <joahnn.1996@gmail.com>
2020-12-09 19:45:25 -0500

    Prueba Jenkisfile completa integracion con Jenkins

--- spring-boot-backend-apirest/src/main/java/com/focus/spingboot/backend/apirest/controllers/ClienteRestController.java
+++ spring-boot-backend-apirest/src/main/java/com/focus/spingboot/backend/apirest/controllers/ClienteRestController.java
@@ -13,7 +13,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = {"http://localhost"})
+@CrossOrigin
 @RestController
 @RequestMapping("/api")
 public class ClienteRestController {
